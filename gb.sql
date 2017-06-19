95412eaff25ab2022f03c721fbde878dc68c348a
desnodiver <desnodiver@gmail.com>
2017-06-19 12:49:58 +0300

    Add files via upload

---
+++ gb.sql
@@ -0,0 +1,23 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+/*!40000 ALTER TABLE `gb` DISABLE KEYS */;
+INSERT INTO `gb` (`id`, `username`, `mail`, `dt`, `msg`) VALUES
+	(7, 's', 's', '2017-06-16 15:29:26', 's'),
+	(8, 'xv', 'xvb', '2017-06-16 15:32:26', 'xv'),
+	(9, 's', 'f', '2017-06-16 15:32:56', 'g'),
+	(10, 'a', 'd', '2017-06-16 15:37:24', 'f'),
+	(11, 'r', 'r', '2017-06-16 15:42:09', 'r'),
+	(12, 'd', 'dg', '2017-06-16 15:47:40', 'dfg'),
+	(13, 'as', 's', '2017-06-18 12:31:29', 'sfd'),
+	(14, 'df', 'd', '2017-06-18 12:36:07', 'df'),
+	(15, 'd', 'd', '2017-06-18 13:49:54', 'fdf'),
+	(16, 'ssd', 'saf', '2017-06-18 15:08:49', 'asdasd');
+/*!40000 ALTER TABLE `gb` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
